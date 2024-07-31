10637d0d3544d9fc07bf5e54f2078e81eb8a8502
Lara <114432707+aviculara@users.noreply.github.com>
2024-07-31 17:19:38 +0300

    roaming and following

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     private Vector3 startingPosition; //of the game object. don't change in code
-    private Vector3 roamingPosition;
+    private Vector3 movingPosition;
     private State state = State.Roam;
     public int direction = 0;
 
     [SerializeField] float speed = 2.5f;
     [SerializeField] float attackRange;
+    [SerializeField] float aggroRange;
     [SerializeField] float deaggroRange = 50f;
+    [SerializeField] float lineOfSightRange = 10f;
     [SerializeField] Transform player;
     //[SerializeField] bool ranged = true;
 
-    private Vector3[] directionPoints = new Vector3[8];
-    private float[] distanceToPlayer = new float[8];
+    //private Vector3[] directionPoints = new Vector3[8];
+    private Vector3[] directionVectors = new Vector3[8];
+    private float[] distanceToTarget = new float[8];
     private int[] directionWeights = new int[8];
+
+    float minDistance = 1f;
 
     public enum State
     {
@@ -28,27 +33,51 @@ public class EnemyAI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        SetDirectionVectors();
         startingPosition = transform.position;
-        //roamingPosition = GetRoamingPosition();
+        //movingPosition = GetRoamingPosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, roamingPosition, speed * Time.deltaTime);
-        float minDistance = 1f;
-        if(Vector3.Distance(transform.position, roamingPosition) < minDistance)
+        DebugDrawDirectionWeights();
+        //DebugDrawRanges();
+        switch(state)
         {
-            roamingPosition = TempRoamingPosition();
+            default:
+                transform.position = Vector3.MoveTowards(transform.position, movingPosition, speed * Time.deltaTime);     
+                if(Vector3.Distance(transform.position, movingPosition) < minDistance)
+                {
+                    movingPosition = TempRoamingPosition();
+                }
+                //check if player is nearby
+                FindTarget();
+                break;
+            case State.Attack:
+                //print("pew pew");
+                FindTarget();
+                break;
+            case State.Follow:
+                
+                transform.position = Vector3.MoveTowards(transform.position, movingPosition, speed * Time.deltaTime);
+                if(Vector3.Distance(transform.position, movingPosition) < minDistance)
+                {
+                    movingPosition = NewFollowingDirection();
+                }
+                FindTarget();
+                break;
         }
+        
+        //OLD VERSION
         //switch(state)
         //{
         //    default:
-        //        transform.position = Vector3.MoveTowards(transform.position, roamingPosition, speed * Time.deltaTime);
+        //        transform.position = Vector3.MoveTowards(transform.position, movingPosition, speed * Time.deltaTime);
         //        float minDistance = 1f;
-        //        if(Vector3.Distance(transform.position, roamingPosition) < minDistance)
+        //        if(Vector3.Distance(transform.position, movingPosition) < minDistance)
         //        {
-        //            roamingPosition = GetRoamingPosition();
+        //            movingPosition = GetRoamingPosition();
         //        }
         //        //FindTarget();
         //        break;
@@ -67,10 +96,21 @@ public class EnemyAI : MonoBehaviour
 
     private Vector3 TempRoamingPosition()
     {
-        float randomDistance = Random.Range(2f, 10f);
-        return transform.position + EightDirectionsToVector(WeightedRandomDirection()) * randomDistance;
+        float randomDistance = Random.Range(2f, 5f);
+        return transform.position + WeightedRandomDirection() * randomDistance;
     }
 
+    private Vector3 NewFollowingDirection(Transform target = null)
+    {
+        if(target == null)
+        {
+            target = player;
+        }
+        float randomDistance = Random.Range(2f, 5f);
+        return transform.position + WeightedRandomDirection(target) * randomDistance;
+    }
+
+    #region old
     public Vector3 RandomDirection()
     {
         //characters are on x-z plane
@@ -107,37 +147,54 @@ public class EnemyAI : MonoBehaviour
         return directionVector;
     }
 
+    #endregion
+
     private void FindTarget()
     {
+        //change behaviour based on player distance
         //print(Vector3.Distance(player.position, transform.position));
         switch(state)
         {
-            default:
+            case State.Roam:
                 if(Vector3.Distance(player.position, transform.position) <= attackRange)
                 {
                     state = State.Attack;
                     print("I'm attacking.");
                 }
+                else if(Vector3.Distance(player.position, transform.position) <= aggroRange)
+                {
+                    state = State.Follow;
+                    print("Found the player and following.");
+                }
+                break;
+            case State.Follow:
+                if(Vector3.Distance(player.position, transform.position) <= attackRange)
+                {
+                    state = State.Attack;
+                    print("I'm attacking.");
+                }
+                else if(Vector3.Distance(player.position, transform.position) > deaggroRange)
+                {
+                    state = State.Roam;
+                    print("back to roaming");
+                }
                 break;
             case State.Attack:
-                //add later: once aggro'd go into following. can de-aggro once the player is too far or gone for too long
+                //add later: de-aggro once the player is gone for too long
                 //add later: if ranged enemy, can run away when player gets too close
-                //if(Vector3.Distance(player.position, transform.position) > attackRange)
-                //{
-                //    state = State.Follow;
-                //    print("back to roaming");
-                //}
-                //else if(Vector3.Distance(player.position, transform.position) > deaggroRange)
-                //{
-                //    state = State.Roam;
-                //}
+                if(Vector3.Distance(player.position, transform.position) > attackRange)
+                {
+                    state = State.Follow;
+                }
+                break;
+            default:
                 state = State.Roam;
                 break;
         }
 
     }
 
-    public Vector3 EightDirectionsToVector(int directionCode)
+    private Vector3 EightDirectionsToVector(int directionCode)
     {
         Vector3 directionVector;
         switch(directionCode)
@@ -205,12 +262,59 @@ public class EnemyAI : MonoBehaviour
         return dir;
     }
 
-    public int WeightedRandomDirection()
+    private void SetDirectionVectors()
     {
+        for(int i = 0; i < directionVectors.Length; i++)
+        {
+            directionVectors[i] = EightDirectionsToVector(i);
+        }
+    }
+
+    //public int WeightedRandomDirection()
+    //{
+    //    //calculate the position of the 8 points, write into the array
+    //    SetDistances(startingPosition);
+    //    //calculate the weight of the 8 points based on their distance to target
+    //    SetDirectionWeights(startingPosition);
+    //    CheckLineOfSight();
+    //    //pick a random direction with the weights
+    //    int totalWeight = 0;
+    //    for(int i = 0; i < directionWeights.Length; i++)
+    //    {
+    //        totalWeight += directionWeights[i];
+    //    }
+    //    int randomValue = Random.Range(0, totalWeight);
+    //    for(int i = 0; i < directionWeights.Length; i++)
+    //    {
+    //        if(randomValue < directionWeights[i])
+    //        {
+    //            return i;
+    //        }
+    //        else
+    //        {
+    //            randomValue -= directionWeights[i];
+    //        }
+    //    }
+    //    print("random overshoot");
+    //    return 0;
+    //}
+
+    public Vector3 WeightedRandomDirection(Transform targetTransform = null)
+    {
+        Vector3 targetVector;
+        if(targetTransform == null)
+        {
+            targetVector = startingPosition;
+        }
+        else
+        {
+            targetVector = targetTransform.position;
+        }
         //calculate the position of the 8 points, write into the array
-        SetDirectionPoints(startingPosition);
+        SetDistances(targetVector);
         //calculate the weight of the 8 points based on their distance to target
-        SetDirectionWeights(startingPosition);
+        SetDirectionWeights(targetVector);
+        CheckLineOfSight();
         //pick a random direction with the weights
         int totalWeight = 0;
         for(int i = 0; i < directionWeights.Length; i++)
@@ -218,11 +322,12 @@ public class EnemyAI : MonoBehaviour
             totalWeight += directionWeights[i];
         }
         int randomValue = Random.Range(0, totalWeight);
-        for(int i=0; i < directionWeights.Length; i++)
+        for(int i = 0; i < directionWeights.Length; i++)
         {
             if(randomValue < directionWeights[i])
             {
-                return i;
+                //print("chose direction " + i);
+                return directionVectors[i];
             }
             else
             {
@@ -230,10 +335,9 @@ public class EnemyAI : MonoBehaviour
             }
         }
         print("random overshoot");
-        return 0;
+        return directionVectors[0];
     }
-
-    public void SetDirectionPoints(Vector3 target)
+    public void SetDistances(Vector3 target)
     {
         /* 
          * Calculates the points unit distance away from enemy in 8 directions
@@ -241,10 +345,11 @@ public class EnemyAI : MonoBehaviour
          */
         //Vector3 playerPos = player.transform.position;
         //print(transform.position);
-        for(int i = 0; i < directionPoints.Length; i++)
+        for(int i = 0; i < distanceToTarget.Length; i++)
         {
-            directionPoints[i] = transform.position + EightDirectionsToVector(i);
-            distanceToPlayer[i] = Vector3.Distance(directionPoints[i], target); //this might need to seperate
+            Vector3 directionPoint = transform.position + EightDirectionsToVector(i);
+            distanceToTarget[i] = Vector3.Distance(directionPoint, target); //this might need to seperate
+            //Debug.DrawLine(directionPoints[i], directionPoints[i] + Vector3.up * 0.1f, Color.red);
             //print(directionPoints[i]);
         }
     }
@@ -259,24 +364,33 @@ public class EnemyAI : MonoBehaviour
         int maxDistanceIndex = 0;
         int minDistanceIndex = 0;
         float currentDistance = Vector3.Distance(transform.position, target);
-        for(int i = 0; i < directionPoints.Length; i++)
+        for(int i = 0; i < distanceToTarget.Length; i++)
         {
-            if(distanceToPlayer[i] < distanceToPlayer[minDistanceIndex])
+            if(distanceToTarget[i] < distanceToTarget[minDistanceIndex])
             {
                 minDistanceIndex = i;
             }
-            else if(distanceToPlayer[i] > distanceToPlayer[maxDistanceIndex])
+            else if(distanceToTarget[i] > distanceToTarget[maxDistanceIndex])
             {
                 maxDistanceIndex = i;
             }
 
-            if(currentDistance > distanceToPlayer[i])   //this direction moves enemy closer to player
+            if(currentDistance > distanceToTarget[i])   //this direction moves enemy closer to player
             {
                 directionWeights[i] = 75;
             }
-            else if(currentDistance < distanceToPlayer[i])  //this direction moves enemy further from player
+            else if(currentDistance < distanceToTarget[i])  //this direction moves enemy further from player
             {
-                directionWeights[i] = 25;
+                switch(state)
+                {
+                    default:
+                        directionWeights[i] = 25;
+                        break;
+                    case State.Follow:
+                        directionWeights[i] = 1;
+                        break;
+                }
+
             }
             else
             {
@@ -286,5 +400,53 @@ public class EnemyAI : MonoBehaviour
         directionWeights[maxDistanceIndex] = 1;
         directionWeights[minDistanceIndex] = 100;
         //print("best direction is " + minDistanceIndex + " , worst direction is " + maxDistanceIndex);
+
     }
+
+    public void CheckLineOfSight()
+    {
+        /*
+         * reduce weights if obstacle within line of sight
+         */
+        for(int i = 0; i < distanceToTarget.Length; i++ )
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(transform.position, directionVectors[i], out hit, lineOfSightRange))
+            {
+                print("Found an object: " + hit.transform.name);
+                if(hit.transform.CompareTag("Obstacle"))
+                {
+                    directionWeights[i] = 1;
+                }
+                else if(hit.transform.CompareTag("Player"))
+                {
+                    directionWeights[i] = 150;
+                }
+
+            }
+            Debug.DrawRay(transform.position, directionVectors[i] * lineOfSightRange, Color.yellow);
+        }
+        
+
+    }
+
+    private void DebugDrawDirectionWeights()
+    {
+        for(int i = 0; i < directionVectors.Length; i++)
+        {
+            Debug.DrawRay(transform.position, directionVectors[i] * directionWeights[i] /20 , Color.red);
+        }
+    }
+
+    private void DebugDrawRanges()
+    {
+        for(int i = 0; i < directionVectors.Length; i +=2)
+        {
+            Debug.DrawRay(transform.position, directionVectors[i] * attackRange, Color.red);
+            Debug.DrawRay(transform.position, directionVectors[i] * aggroRange, Color.yellow);
+            Debug.DrawRay(transform.position, directionVectors[i] * deaggroRange, Color.cyan);
+        }
+    }
+
 }
